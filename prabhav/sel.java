92d1e35d2edcb13d8da4880d6f8418ad818ef028
prabhav7 <31265769+prabhav7@users.noreply.github.com>
2017-11-02 20:41:31 +0530

    Update sel.java

--- prabhav/sel.java
+++ prabhav/sel.java
@@ -1,9 +1,37 @@
+package snippet;
 
-public class sel {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+public class selection_sort {
+	public static void selection_Sort(int []arr) {  
+	  
+    for (int i = 0; i < arr.length - 1; i++)  
+    {  
+        int index = i;  
+        for (int j = i + 1; j < arr.length; j++){  
+            if (arr[j] < arr[index]){  
+                index = j;
+            }  
+        }  
+        int smallerNumber = arr[index];   
+        arr[index] = arr[i];  
+        arr[i] = smallerNumber;  
+    }  
+}  
+   
+public static void main(String a[]){ 
+	
+    int[] arr1 = {9,14,3,2,43,11,58,22};  
+    System.out.println("Before Selection Sort");  
+    for(int i:arr1){  
+        System.out.print(i+" ");  
+    }  
+    System.out.println();  
+      
+    selection_Sort(arr1);
+     
+    System.out.println("After Selection Sort");  
+    for(int i:arr1){  
+        System.out.print(i+" ");  
+    }  
+}  
+ 
 }
